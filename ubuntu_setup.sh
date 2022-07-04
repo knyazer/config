aaa0a04317eafb9a4410d8350955634c80ebbe48
Knyaz Roma <mail@knyaz.tech>
2022-07-04 20:17:43 +0200

    Spotify and telegram

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -50,18 +50,24 @@ apt_install qbittorrent || true
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub io.bassi.Amberol || true
 
-# vscode - backup text editor
+# vscode - text editor
 source ubuntu_vscode_install.sh
 
 # CLion - C/C++ IDE
 snap install clion --classic || true
+
+# Telegram desktop client
+snap install telegram-desktop
+
+# Spotify
+snap install spotify
 
 # Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
 dpkg -i google-chrome-stable_current_amd64.deb || true
 rm -f google-chrome-stable_current_amd64.deb || true
 
-# Sublime - main text editor
+# Sublime - text editor
 wget -O- https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | tee /usr/share/keyrings/sublimehq.gpg || true
 echo 'deb [signed-by=/usr/share/keyrings/sublimehq.gpg] https://download.sublimetext.com/ apt/stable/' | sudo tee /etc/apt/sources.list.d/sublime-text.list
 apt-get update
