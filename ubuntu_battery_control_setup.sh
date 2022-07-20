3b57d9bc2f6a08d096d24f6f445206bbcc234bae
Roma Knyaz <git@knyaz.tech>
2022-07-20 16:04:02 +0200

    TLP recommendations + my main config file

--- ubuntu_battery_control_setup.sh
+++ ubuntu_battery_control_setup.sh
@@ -7,9 +7,13 @@ source misc.sh
 add-apt-repository ppa:linrunner/tlp || true
 apt_install tlp acpi-call-dkms tp-smapi-dkms || true
 change_config_field_value /usr/share/tlp/defaults.conf USB_AUTOSUSPEND 0
-systemctl enable tlp.service || true
-tlp setcharge 0 60 || true
-tlp start || true
+
+# Also copy the entire tlp config file
+cp -f tlp.conf /etc
 
 # Disable default thingy as it breaks everything
-sudo systemctl disable power-profiles-daemon.service
+sudo systemctl disable power-profiles-daemon.service || true
+
+systemctl enable tlp.service || true
+tlp start || true
+
